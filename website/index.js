b5c4f71a006b90a702edbc50faf44648cc65be7d
Ankit Chowdhury <ankitchowdhury550@gmail.com>
2024-07-06 11:53:34 +0530

    1st

---
+++ website/index.js
@@ -0,0 +1,101 @@
+// console.log(`hello world`);
+// console.log(`i like pizza`);
+
+// window.alert(`this is an alert`);
+// window.alert(`i like pizza`)
+
+//this is a comment
+
+/*
+  this is a multi line comment
+*/
+
+// document.getElementById("myh1").textContent="hello";
+// document.getElementById("myp").textContent="i like pizza";
+
+
+//variable =A container that stores a value behaves as if it were the value it contains
+
+//1. declaration---> let x;
+//2. assignment ---> x=100;
+
+// let age =25;
+// let price=10.99;
+// let gpa =2.1;
+
+// console.log(typeof age)
+// console.log(typeof price)
+// console.log(typeof gpa)
+// console.log(age)
+// console.log(`you are ${age} years old`)
+// console.log(`the price is ${price}`)
+// console.log(`your gpa is ${gpa}`);
+
+// let firstname = "ankit"
+// let favoritefood = "pizza";
+// let email="ankit@gmail.com"
+
+// console.log(typeof firstname)
+// console.log(firstname)
+// console.log(`your name is ${firstname}`)
+// console.log(`your favorite food is ${favoritefood}`)
+// console.log(`your email is ${email}`)
+
+// let isonline ="false"
+// let forsale="true"
+// let isstudent="true"
+// console.log(`bro is online : ${isonline}`)
+
+// let fullname="ankit"
+// let age=25;
+// let isstudent=true;
+
+// document.getElementById("p1").textContent=fullname;
+// document.getElementById("p2").textContent=age;
+// document.getElementById("p3").textContent=isstudent;
+
+// document.getElementById("p1").textContent=`your name is ${fullname}`;
+// document.getElementById("p2").textContent=`you are ${age} years old`;
+// document.getElementById("p3").textContent=`enrolled: ${isstudent}`;
+
+//arithmetic operators = operands(values,variables etc)
+//                       operators(+ - * /)
+
+// let student=32;
+
+// student=student +1;
+// student=student -1;
+// student=student *2;
+// student=student /2;
+// student=student **2;
+// let extrastudent = student %3
+// console.log(extrastudent)
+
+//augemented assignment operators
+
+// student += 2;
+// student -= 2;
+// student *= 2;
+// student /= 2;
+// student **= 2;
+// student %= 2;
+
+// student++;
+// student--;
+
+// console.log(student)
+
+/*
+    operator precedence
+    1.prenthesis()
+    2. exponents
+    3. multiplication
+    4.addition and substraction
+*/
+// let result = 1 + 2 * 3 + 4 ** 2;
+// let result = 12 % 5 + 8 /2
+// let result =6/2 ** (2+5)
+
+// console.log(result)
+
+
