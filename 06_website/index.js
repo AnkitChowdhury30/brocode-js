1e39a6eb4c05a74e07610b1e9042120aafa6a8a3
Ankit Chowdhury <ankitchowdhury550@gmail.com>
2024-07-10 23:34:35 +0530

    10-07-2024

--- 06_website/index.js
+++ 06_website/index.js
@@ -26,3 +26,46 @@ mysubmitbtn.onclick=function(){
          paymentrsult.textContent=`you must have to select a payment type`
     }
 }
+//++++++++++++++++++++++++++
+//ternary operator ? :;
+//++++++++++++++++++++++++++
+//switch case same as cpp
+//++++++++++++++++++++++++++
+//string method
+
+let username="   ankitnkkk"
+console.log(username.charAt(0));
+console.log(username.indexOf("a"));
+console.log(username.lastIndexOf("n"));
+console.log(username.length);
+
+username=username.trim();
+console.log(username);
+
+username=username.toUpperCase();
+console.log(username);
+
+username=username.repeat(3);
+console.log(username);
+
+result=username.startsWith('a');
+console.log(result)
+
+result=username.endsWith('');
+console.log(result);
+
+let phnum= "123-456-789";
+phnum=phnum.replaceAll("-","");
+console.log(phnum);
+
+let name="ankit";
+name=name.padStart(15,"*");
+console.log(name);
+
+let k="abjs"
+k=k.padEnd(15,"&");
+console.log(k);
+
+//++++++++++++++++++++++++
+
+//string slicing = creating a substring from aportion of string 
